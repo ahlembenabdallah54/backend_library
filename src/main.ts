10d278d520f0fe7b3e06e5a4fb5a48c2d6454438
Islem Labidi <labidiislem971@gmail.com>
2026-04-15 12:20:34 +0100

    connect backend to frontend

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    // CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
